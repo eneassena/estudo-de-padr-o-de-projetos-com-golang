60253ec5f0af67209f5e57387db32e8043bca680
eneassena <desenvolvedoreneas2000@gmail.com>
2023-01-09 00:05:30 -0300

    start project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eneassena/estudo-de-padr-o-de-projetos-com-golang
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Designer Patterns with Golang")
+}
